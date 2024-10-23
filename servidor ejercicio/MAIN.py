612682915d019e8d679d396c82f22d259b0a2a5f
Noah619 <noah_orellana@euneiz.com>
2024-10-23 11:42:18 +0200

    Cambio de servidores a cliente y servidor

--- servidor ejercicio/MAIN.py
+++
@@ -1,9 +0,0 @@
-from src.servidor.Funciones_servidor import iniciar_servidor 
-
-# Configuración del servidor
-HOST = '127.0.0.1'  # Dirección IP local (localhost)
-PORT = 12345        # Puerto del servidor
-
-
-if __name__ == "__main__":
-    iniciar_servidor(HOST, PORT)
